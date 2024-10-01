f3fe52543cfafcacc482fa851f9a2e88a6751cab
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-10-02 00:02:57 +0530

    localhost server

---
+++ main.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req,res)=>{
+    fs.readFile("./sample.txt", (err, data)=> {
+        res.end(data);
+    });
+});
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
