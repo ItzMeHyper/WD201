0d2c0b00326d97d37c6a3da517bd9f5cd36bae67
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-10-02 11:15:58 +0530

    Package updated

--- http-server/index.js
+++ http-server/index.js
@@ -3,27 +3,27 @@ const fs = require("fs");
 const minimist = require("minimist");
 
 const args = minimist(process.argv.slice(2));
-const PORT = args.port || 5000;
+const PORT = args.port || 3000;
 
 let homeContent = "";
 let projectContent = "";
 let registrationContent = "";
 
-fs.readFile("./http-server/home.html", (err, home) => {
+fs.readFile("./home.html", (err, home) => {
     if (err) {
         throw err;
     }
     homeContent = home;
 });
 
-fs.readFile("./http-server/project.html", (err, project) => {
+fs.readFile("./project.html", (err, project) => {
     if (err) {
         throw err;
     }
     projectContent = project;
 });
 
-fs.readFile("./http-server/registration.html", (err, registration) => {
+fs.readFile("./registration.html", (err, registration) => {
     if (err) {
         throw err;
     }
@@ -34,19 +34,31 @@ const server = http.createServer((request, response) => {
     const url = request.url;
     response.writeHead(200, { "Content-Type": "text/html" });
 
-    switch (url) {
-        case "/project":
-            response.write(projectContent);
+    if (request.method === 'POST' && url === '/register') {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString(); // Convert Buffer to string
+        });
+        request.on('end', () => {
+            console.log('Registration Data:', body);
+            response.write('<h1>Registration Successful!</h1>');
             response.end();
-            break;
-        case "/registration":
-            response.write(registrationContent);
-            response.end();
-            break;
-        default:
-            response.write(homeContent);
-            response.end();
-            break;
+        });
+    } else {
+        switch (url) {
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
     }
 });
 
